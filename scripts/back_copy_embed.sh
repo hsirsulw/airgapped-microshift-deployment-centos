e74fc75a01b16b637727fa635f9901263fc07465
Hrushabh Sirsulwar <38070234+hsirsulw@users.noreply.github.com>
2026-02-07 19:26:45 +0530

    Delete scripts/back_copy_embed.sh

--- scripts/back_copy_embed.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eux -o pipefail
-while IFS="," read -r img sha ; do
-
-   skopeo copy --preserve-digests "dir:/usr/lib/containers/storage/${sha}" "containers-storage:${img}"
-
-done < "/usr/lib/containers/storage/image-list.txt"
